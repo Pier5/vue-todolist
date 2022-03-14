18e64767dc696ca50e7fd11451811e3eef46012e
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-14 16:11:43 +0100

    -milestone 3-

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul 
-// pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
-// che quindi viene aggiunto alla lista dei todo esistenti.
 // Bonus:
 // 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere 
 // il todo alla lista
@@ -15,7 +8,7 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        classList : 'text-done',
+        newTodo: '',
         arrTodo: [
             {
                 text: 'Fare i compiti',
@@ -34,8 +27,13 @@ const app = new Vue ({
     methods: {
         deleteTodo(index) {
             this.arrTodo.splice(index, 1);
-
-        }
+        },
+        addTodo() {
+            if(this.newTodo.trim() != '') {
+                this.arrTodo.unshift({text: this.newTodo});
+                this.newTodo = '';
+            }
+        },
 
     }
 })
